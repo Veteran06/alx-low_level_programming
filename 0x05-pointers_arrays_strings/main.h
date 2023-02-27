e80314c68cc36b55351838013f6514e3c54c1f2c
Veteran06 <manuelmunyua@gmail.com>
2023-02-27 14:30:51 -0800

    task 8

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+
 int _putchar(char c);
 
 void reset_to_98(int *n);
@@ -20,3 +26,4 @@ void print_array(int *a, int n);
 
 char *_strcpy(char *dest, char *src);
 
+#endif
