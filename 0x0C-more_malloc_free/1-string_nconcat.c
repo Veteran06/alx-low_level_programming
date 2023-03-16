a9e28cc032c28c2b4dcb6713ae6d1fda6106f644
Veteran06 <manuelmunyua@gmail.com>
2023-03-16 11:22:51 -0700

    task 1 more malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -22,7 +22,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n < k2)
 		s = malloc(sizeof(char) * (k1 + n + 1));
 	else
-		s = malloc(sizeof(char) * (k2 + k2 + 1));
+		s = malloc(sizeof(char) * (k1 + k2 + 1));
 
 	if (!s)
 		return (NULL);
