eb1c6d3470942a084871a1cf66ce61b2948a44f5
Veteran06 <manuelmunyua@gmail.com>
2023-03-22 14:26:48 -0700

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,7 +5,7 @@
  * print_name - name using pointer to function
  * @name: string to add
  * @f: pointer to function
- * Return: nothing
+ * Return: name
  */
 void print_name(char *name, void (*f)(char *))
 {
