07d182f7374c19935b1a2dd6fb18c8c7fea9d69d
Veteran06 <manuelmunyua@gmail.com>
2023-03-22 14:00:03 -0700

    task 3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -8,8 +8,8 @@ int op_mod(int a, int b);
 
 /**
  * op_add - returns the sum of two integers
- * @a: first integer
- * @b: second integer
+ * @a: first number
+ * @b: second number
  * Return: sum of a and b
  */
 int op_add(int a, int b)
@@ -19,8 +19,8 @@ int op_add(int a, int b)
 
 /**
  * op_sub - returns the difference
- * @a: first integer
- * @b: second integer
+ * @a: first number
+ * @b: second number
  * Return: difference between a and b
  */
 int op_sub(int a, int b)
@@ -30,8 +30,8 @@ int op_sub(int a, int b)
 
 /**
  * op_mul -  returns the product of a and b
- * @a: first integer
- * @b: second integer
+ * @a: first number
+ * @b: second number
  * Return: product of a and b
  */
 int op_mul(int a, int b)
@@ -41,8 +41,8 @@ int op_mul(int a, int b)
 
 /**
  * op_div - result of the division of a by b
- * @a: first integer
- * @b: second integer
+ * @a: first number
+ * @b: second number
  * Return: division of a and b
  */
 int op_div(int a, int b)
@@ -52,8 +52,8 @@ int op_div(int a, int b)
 
 /**
  * op_mod - returns the remainder of the division of a by b
- * @a: first integer
- * @b: second integer
+ * @a: first number
+ * @b: second number
  * Return: remainder of the division of a by b
  */
 int op_mod(int a, int b)
