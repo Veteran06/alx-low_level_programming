d65c1dd57ccad47cc872054b2b428c165adcec00
Veteran06 <manuelmunyua@gmail.com>
2023-03-30 13:45:22 -0700

    advanced 1

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - function executed before main
+ * Return: no return
+ */
+void _attribute_((constructor)) main()
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",/nl bore my house upon my back!/n".);
+}
