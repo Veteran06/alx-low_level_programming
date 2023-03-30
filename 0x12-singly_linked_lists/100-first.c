d9315e37360823a2de915ddf02e6354b241379d4
Veteran06 <manuelmunyua@gmail.com>
2023-03-30 14:11:36 -0700

    corrections

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,6 +6,6 @@
  */
 void __attribute__((constructor)) bmain()
 {
-	printf("You're beat! and yet, you must allow");
-	printf(",\nl bore my house upon my back!/n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf(" bore my house upon my back!/n");
 }
