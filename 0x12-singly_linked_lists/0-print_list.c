8fd6e9d0a956cfeabde31c458947f2a9c0a8b7bb
Veteran06 <manuelmunyua@gmail.com>
2023-03-30 14:06:11 -0700

    task 1 correction

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,9 +9,9 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t s;
+	size_t w;
 
-	s = 0;
+	w = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
@@ -19,7 +19,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		s++;
+		w++;
 	}
-	return (s);
+	return (w);
 }
