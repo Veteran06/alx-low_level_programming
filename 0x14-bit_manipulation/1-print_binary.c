3e2fa0c902b2d29d4f2bb26f42375126c85b2e4e
Veteran06 <manuelmunyua@gmail.com>
2023-04-06 09:41:38 -0700

    second task

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,17 +10,18 @@ void print_binary(unsigned long int n)
 	unsigned long int w;
 	int e, k = 0;
 
-	w = n >> e;
-	for (e = 10; e >= 0; e--)
+	for (e = 10; e >=0; e--)
 	{
+		w = n >> e;
+	
 		if (w & 1)
 		{
-			_putchar("1");
+			_putchar('1');
 			k++;
 		}
 		else if (k)
-			_putchar("0");
+			_putchar('0');
 	}
 	if (!k)
-		_putchar("0");
+		_putchar('0');
 }
