8dfee2936bbd7acd2b326a3b66425652d8796609
Veteran06 <manuelmunyua@gmail.com>
2023-04-10 10:50:33 -0700

    task 1 0x15

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,8 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);/* 0600 is permission*/
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	/* 0600 is permission*/
 
 	if (fd == -1)
 		return (-1);
