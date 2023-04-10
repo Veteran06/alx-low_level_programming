a06c629b59603f41e4c57bec90b7681f6192552f
Veteran06 <manuelmunyua@gmail.com>
2023-04-10 07:56:08 -0700

    task 1 0x15

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	/*
 	 *  to create if none, truncate if it exists, and write only,
 	 * 0600 gives permission to owner to write and read
