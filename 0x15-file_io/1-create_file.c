69f3bbc4285fe57723402b1530c5d93382e5de63
Veteran06 <manuelmunyua@gmail.com>
2023-04-10 07:53:50 -0700

    task 1 0x15

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 		text_content = " ";
-	
+
 	for (w = 0; text_content[w]; w++)
 		wr = write(fd, text_content, w);
 
